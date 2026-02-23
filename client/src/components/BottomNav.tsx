ec75079b1a25206d08badb7ed7b11813b510aa9f
ibr25ahima-de <ibr25ahima@gmail.com>
2026-02-23 13:52:54 +0000

    Add files via upload

---
+++ client/src/components/BottomNav.tsx
@@ -0,0 +1,35 @@
+import { Home, Search, PlusSquare, MessageCircle, User } from "lucide-react";
+import { useLocation } from "wouter";
+
+export default function BottomNav() {
+  const [location, navigate] = useLocation();
+
+  const Item = ({ icon: Icon, path }: any) => (
+    <button
+      onClick={() => navigate(path)}
+      className={`flex flex-col items-center text-xs ${
+        location === path ? "text-white" : "text-gray-400"
+      }`}
+    >
+      <Icon size={26} />
+    </button>
+  );
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 flex justify-around py-2 z-50">
+      <Item icon={Home} path="/feed" />
+      <Item icon={Search} path="/discover" />
+
+      {/* center upload */}
+      <button
+        onClick={() => navigate("/upload")}
+        className="bg-white text-black px-3 rounded-md"
+      >
+        <PlusSquare size={28} />
+      </button>
+
+      <Item icon={MessageCircle} path="/inbox" />
+      <Item icon={User} path="/profile" />
+    </div>
+  );
+}
