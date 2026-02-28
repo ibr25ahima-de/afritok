b030c60fdc31cf3e93af9c2d235ecde967092e7e
ibr25ahima-de <ibr25ahima@gmail.com>
2026-02-28 14:51:17 +0000

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
-import { trpc } from "@/lib/trpc";
+import { UploadProvider } from "@/contexts/UploadContext";
+importt { trpc } from "@/lib/trpc";
 import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
@@ -55,7 +56,9 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <UploadProvider>
+  <App />
+</UploadProvider>
     </QueryClientProvider>
   </trpc.Provider>
 );
