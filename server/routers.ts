262ae55abc2f167e3a496cd9d40ff266ef6a5e46
ibr25ahima-de <ibr25ahima@gmail.com>
2026-03-18 13:36:58 +0000

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,5 @@
-import { COOKIE_NAME } from "@shared/const";
+import { instantWithdrawalRouter } from "./instant-withdrawal-router";
+importt { COOKIE_NAME } from "@shared/const";
 import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, router, protectedProcedure } from "./_core/trpc";
