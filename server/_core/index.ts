5ac8dbeae6d25571a6d7201f332d19d4cf50f99c
ibr25ahima-de <ibr25ahima@gmail.com>
2026-03-27 14:13:26 +0000

    Update index.ts

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -108,9 +108,8 @@ async function startServer() {
 
   const port = parseInt(process.env.PORT || "3000");
 
-  server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-}
+  server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
 
 startServer().catch(console.error);
