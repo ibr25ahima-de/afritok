74d0abd2d8dda22ef6648eaa2ed794e69717a2e4
ibr25ahima-de <ibr25ahima@gmail.com>
2026-04-14 14:30:08 +0000

    Add files via upload

---
+++ server/routers-feed.ts
@@ -0,0 +1,17 @@
+import { router, publicProcedure } from "./_core/trpc";
+import { z } from "zod";
+import { getFeedVideos } from "./db";
+
+export const feedRouter = router({
+  getFeed: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().default(10),
+        offset: z.number().default(0),
+      })
+    )
+    .query(async ({ input }) => {
+      const videos = await getFeedVideos(input.limit, input.offset);
+      return videos;
+    }),
+});
