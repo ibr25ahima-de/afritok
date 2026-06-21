f6992d2b48a1df753b65a2460ec290dcd7f2653e
ibr25ahima-de <ibr25ahima@gmail.com>
2026-06-21 19:12:09 +0000

    Add beauty effect to filter in Upload component

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -262,7 +262,9 @@ export default function Upload() {
     // Add effects
     if (activeEffect.id === "vhs") filter += " hue-rotate(90deg) contrast(1.5)";
     if (activeEffect.id === "portrait") filter += " contrast(1.2) saturate(1.1)";
-    
+    if (activeEffect.id === "beauty") {
+  filter += " blur(0.5px) brightness(110%) saturate(120%)";
+}
     return filter;
   };
 
