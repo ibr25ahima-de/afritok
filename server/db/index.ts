0d297c494a5ae019204c67004ada461d1dcefa7d
ibr25ahima-de <ibr25ahima@gmail.com>
2026-07-31 10:31:30 +0000

    Add files via upload

---
+++ server/db/index.ts
@@ -0,0 +1,27 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export const db = drizzle(pool);
+
+// Export all functions from sub-modules
+export * from "./users";
+export * from "./videos";
+export * from "./likes";
+export * from "./comments";
+export * from "./favorites";
+export * from "./shares";
+export * from "./followers";
+export * from "./earnings";
+export * from "./withdrawals";
+export * from "./otp";
+export * from "./settings";
+export * from "./profile";
