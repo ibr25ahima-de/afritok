43cf041b41ce2946fbd017fd5f4ebb844bbb5805
ibr25ahima-de <ibr25ahima@gmail.com>
2026-08-10 21:53:07 +0000

    Delete server/routers/admin/finance.ts

--- server/routers/admin/finance.ts
+++
@@ -1,6 +0,0 @@
-import { router, protectedProcedure } from "../../_core/trpc";
-import { TRPCError } from "@trpc/server";
-
-export const financeRouter = router({
-  // Financial statistics routes
-});
